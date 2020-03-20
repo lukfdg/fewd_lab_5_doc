7920785fea636e1bac81dc82a2d2373174322653
Lucas_Souza <lukfdg@gmail.com>
2020-03-20 02:41:54 +0000

    ‘commit’

--- js/script.js
+++ js/script.js
@@ -1,8 +1,51 @@
+alert("Welcome to Eat Worthy");
+
+var a;
+function myFunction()
+{
+    if(a==1)
+    {
+        document.getElementById("TableMenu").style.display="";
+        return a=0;
+    }
+
+    else
+    {
+        document.getElementById("TableMenu").style.display="inline";
+    }
+}
+
+/* 
+function myFunction() {
+  var x = document.getElementById("Show Menu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+*/
+
+/*
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
+
+button.onclick - function(){
+    if (content.className == "open");
+    content.className = "";
+    button.innerHTML = "show more";
+    { else }
+    content.className = "open";
+    button.innerHTML = "show less";
+}
+**/
+
 var msg = '<h2>Opening Times</h2><p>Mon-Fri: ' + window.innerWidth + '</p>';
 msg += '<p>Sat-Sun: ' + window.innerHeight + '</p>';
-msg += '<h2>Menu</h2><p>items: ' + window.history.length + '</p>';
+msg += '<h2>Menu</h2><p>items: ' + window.randomNum + '</p>';
 msg += '<h2>Screen</h2><p>width: ' + window.screen.width + '</p>';
 msg += '<p>height: ' + window.screen.height + '</p>';
+
 
 var el = document.getElementById('info');
 el.innerHTML = msg;
@@ -16,34 +59,19 @@ msg2 += '<b>last modified: </b>' + document.lastModified + '</p>';
 var el2 = document.getElementById('footer');
 el2.innerHTML = msg2;
 
-// Generate random number between 1 and 10
 var randomNum = Math.floor((Math.random() * 100) + 1);
 
 var el3 = document.getElementById('info');
 el3.innerHTML += '<h2>Tables Avaliable</h2><p>' + randomNum + '</p>';
 
-// Create a variable to hold a new Date object (defaults to now)
 var today = new Date();
-// Create a variable to hold the year this year
 var year = today.getFullYear();
-
-// Create a variable called el to hold the element whose id attribute has a value of footer
 var el4 = document.getElementById('footer');
-// Write the year into that element.
 el4.innerHTML += '<p>Copyright &copy;' + year + '</p>';
-
-// Create a variable to hold a new Date object (defaults to now)
 var today = new Date();
-// Get the year this year
 var year = today.getFullYear();
-// Set the date that the company was established
 var est = new Date('Apr 16, 1996 15:45:55');
-// Get difference between then & now in milliseconds
 var difference = today.getTime() - est.getTime();
-// Divide by number of milliseconds to get years
 difference = (difference / 31556900000);
-
-// Create a variable called elMsg to hold the element whose id attribute has a value of message
 var elMsg = document.getElementById('message');
-// Write the message into that element.
 elMsg.textContent = Math.floor(difference) + ' years putting art on the plate';
